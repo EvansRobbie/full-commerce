661ddc3f610b8f3598462e1f722c1b98c202c96e
EvansRobbie <evansitoden94@gmail.com>
2023-09-23 20:53:53 +0300

    db connectinin

---
+++ src/app/api/product/route.ts
@@ -0,0 +1,16 @@
+import { dbConnect } from "@/backend/config/dbConnect"
+import Product from "@/backend/models/Product"
+import { NextResponse } from "next/server"
+
+export const POST = async (req:Request) =>{
+    const data = await req.json()
+    await dbConnect()
+    try {
+        const newProduct = await Product.create(data)
+        return NextResponse.json(newProduct, {status:201})
+
+    } catch (error) {
+        console.log(error)
+        return new NextResponse('Failed to create Product', {status:400})
+    }
+}
