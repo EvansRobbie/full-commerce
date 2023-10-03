6c175dd3303242a6bde1e70417a1d9079d2d7885
Evans Robbie <evansitoden94@gmail.com>
2023-10-03 10:59:37 +0300

    search products on typing without clicking the search button

--- src/components/layout/Search.tsx
+++ src/components/layout/Search.tsx
@@ -1,31 +1,40 @@
-'use client'
-import React, {useState, useEffect, FormEvent} from "react";
-import axios from 'axios'
+"use client";
+import React, { useState, useEffect, FormEvent, useCallback } from "react";
+import axios from "axios";
 import { useRouter } from "next/navigation";
 
 const Search = () => {
-    const [keyword, setKeyword] = useState('')
-    const router = useRouter()
+  const [keyword, setKeyword] = useState("");
+  const router = useRouter();
 
-    const handleSearch = (e:FormEvent<HTMLFormElement>) =>{
-      e.preventDefault()
-        if (keyword){
-          router.push(`/?keyword=${keyword}`)
-        }else{
-          router.push('/')
-        }
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    search();
+  };
+  const search = useCallback(() => {
+    if (keyword) {
+      router.push(`/?keyword=${keyword}`);
+    } else {
+      router.push("/");
     }
+  }, [keyword, router]);
+  useEffect(() => {
+    search();
+  }, [search]);
   return (
-    <form onSubmit={handleSearch} className="flex flex-nowrap items-center w-full order-last md:order-none mt-5 md:mt-0 md:w-2/4 lg:w-2/4">
+    <form
+      onSubmit={handleSearch}
+      className="flex flex-nowrap items-center w-full order-last md:order-none mt-5 md:mt-0 md:w-2/4 lg:w-2/4"
+    >
       <input
         className="flex-grow appearance-none border border-gray-200 bg-gray-100 rounded-md mr-2 py-2 px-3 hover:border-gray-400 focus:outline-none focus:border-gray-400"
         type="text"
         value={keyword}
-        onChange={(e)=>setKeyword(e.target.value)}
+        onChange={(e) => setKeyword(e.target.value)}
         placeholder="Enter your keyword"
       />
       <button
-      // onClick={handleSearch}
+        // onClick={handleSearch}
         type="submit"
         className="px-4 py-2 inline-block text-white border border-transparent bg-blue-600 rounded-md hover:bg-blue-700"
       >
